d988aa574e61b5f9daf5ac263012ab7703151540
Gauravmote3328 <gaurav.mote3328@gmail.com>
2023-02-07 17:32:41 +0530

    Student Data set.

--- Demo/src/com/Test.java
+++ Demo/src/com/Test.java
@@ -1,9 +1,16 @@
 package com;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Check feature Create class Branch...");
+
+		Student s = new Student();
+		s.setId(10);
+		s.setName("Gaurav");
+		s.setAddress("Pune");
+		
+		System.out.println("Checked Student data :"+s);
 	}
 
 }
